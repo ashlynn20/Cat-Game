5c5a5cff91f4a59a8cc1620efb278a009b94be08
alynn20 <ahervey999@gmail.com>
2026-06-20 08:37:34 -0600

    base

---
+++ catgame/src/main/java/com/cat/catgame/CatgameApplication.java
@@ -0,0 +1,13 @@
+package com.cat.catgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatgameApplication.class, args);
+	}
+
+}
